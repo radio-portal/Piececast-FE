b4086c0bdb69e8eefd279dd36a1eedf4aaef24d1
Gyuri <rbf0311@gmail.com>
2025-05-31 02:24:59 +0900

    feat: add program page ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { Main, Station, Player } from './pages';
+import { Main, Station, Player, Program } from './pages';
 import { MainLayout } from './route';
 
 const App = () => {
@@ -14,6 +14,7 @@ const App = () => {
           <Route path="/" element={<Main />} />
           <Route path="/station" element={<Station />} />
           <Route path="/player" element={<Player />} />
+          <Route path="/program" element={<Program />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/program/types.ts
@@ -0,0 +1,12 @@
+export type Episode = {
+  id: number;
+  date: string;
+  duration: string;
+}
+
+export type Clip = {
+  id: number;
+  date: string;
+  title: string;
+  duration: string;
+}
