a8ae98cb332ec79fcec73490067a997805f1b785
Gyuri <86146265+rlcz1@users.noreply.github.com>
2025-05-31 03:18:41 +0900

    feat: add guide page for service usage instructions (#14)

--- src/pages/main/Banner.tsx
+++ src/pages/main/Banner.tsx
@@ -1,4 +1,7 @@
+import { useNavigate } from 'react-router-dom';
+
 const Banner = () => {
+  const navigate = useNavigate();
   return (
     <div className="w-full h-[200px] flex justify-between px-[100px] items-center bg-gradient-to-r from-[#4F6FCB] via-[rgba(59,83,152,0.94)] to-[rgba(39,55,101,0.92)]">
       <div className="flex flex-col text-white">
@@ -7,7 +10,12 @@ const Banner = () => {
           <p className="text-[22px] font-semibold mt-[4px]">음악, 사연, 토크까지 한눈에! 놓친 방송, 핵심만 빠르게 듣자!</p>
           <img src="/assets/images/bannerArrow.png" alt="arrow" />
         </div>
-        <button className="bg-gray2 w-[130px] h-[30px] rounded-full text-[14px] font-semibold text-center mt-[20px]">이용방법 안내</button>
+        <button 
+          onClick={() => navigate('/guide')} 
+          className="bg-gray2 w-[130px] h-[30px] rounded-full text-[14px] font-semibold text-center mt-[20px] cursor-pointer"
+        >
+          이용방법 안내
+        </button>
       </div>
       <div className="flex items-center">
         <img src="/assets/images/character2.png" alt="character" className="h-[100px] rounded-[5px] mt-[55px]" />
